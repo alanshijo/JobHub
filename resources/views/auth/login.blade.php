ca109bf4d871b27bdba1fa8456e4dccfa41ed3c5
Alan Shijo <94635672+alanshijo@users.noreply.github.com>
2023-08-02 12:00:33 +0530

    Login & Register done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,14 +21,13 @@
 <section class="site-section">
     <div class="container">
         <div class="row">
-
             <div class="col-md-12">
                 <form action="{{ route('login') }}" method="POST" class="p-4 border rounded">
                     @csrf
                     <div class="row form-group">
                         <div class="col-md-12 mb-3 mb-md-0">
                             <label class="text-black" for="fname">Email</label>
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <input id="email" type="email" placeholder="Enter your valid email address here" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                             @error('email')
                             <span class="invalid-feedback" role="alert">
@@ -40,7 +39,7 @@
                     <div class="row form-group mb-4">
                         <div class="col-md-12 mb-3 mb-md-0">
                             <label class="text-black" for="fname">Password</label>
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                            <input id="password" type="password" placeholder="Enter your secret password here" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                             @error('password')
                             <span class="invalid-feedback" role="alert">
@@ -62,7 +61,7 @@
                     <div class="row form-group">
                         <div class="col-md-12">
                             <button type="submit" class="btn px-4 btn-primary text-white">
-                                {{ __('Login') }}
+                                Log in
                             </button>
 
 
